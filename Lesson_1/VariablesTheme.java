9447f95201b3a26fdd11b3efb625439c7f16ffcd
Pavel Tsukanov <pavel.tsukanov@gmail.com>
2023-06-30 13:55:13 +0300

    Работа над ошибками VariablesTheme.java

--- Lesson_1/VariablesTheme.java
+++ Lesson_1/VariablesTheme.java
@@ -2,29 +2,29 @@ public class VariablesTheme {
 
     public static void main(String[] args) {
         System.out.println("1. Вывод характеристик компьютера.");
-        byte a = 5;
-        short b = 11_300;
-        int c = 3_400_300;
-        long d = 2_147_483_648_808L;
-        float e = 99.88F;
-        double f = 1205.45;
-        char g = 'H';
-        boolean h = true;
-        System.out.println("Тип процессора = " + "Intel i" + a);
-        System.out.println("Модель процессора = " + b + g);
-        System.out.println("Модель материнской платы = " + "Asus " + c);
-        System.out.println("Серийный номер материнской платы = " + d);
-        System.out.println("Текущая скорость шины = " + e + " MHz");
-        System.out.println("Текущая скорость ядра = " + f + " MHz");
-        System.out.println("Исправна = " + h);
+        byte cpuSeries = 5;
+        short cpuModel = 11_300;
+        int mbModel = 3_400_300;
+        long mbSerialNumber = 2_147_483_648_808L;
+        float busSpeed = 99.88F;
+        double coreSpeed = 1205.45;
+        char cpuPrefix = 'H';
+        boolean isActive = true;
+        System.out.println("Тип процессора = " + "Intel i" + cpuSeries);
+        System.out.println("Модель процессора = " + cpuModel + cpuPrefix);
+        System.out.println("Модель материнской платы = " + "ASUS " + mbModel);
+        System.out.println("Серийный номер материнской платы = " + mbSerialNumber);
+        System.out.println("Текущая скорость шины = " + busSpeed + " MHz");
+        System.out.println("Текущая скорость ядра = " + coreSpeed + " MHz");
+        System.out.println("Исправна = " + isActive);
 
         System.out.println("\n2. Расчет стоимости товара со скидкой");
-        int penCost = 100; // стоимость ручки
-        int bookCost = 200; // стоимость книги
-        byte discount = 11; // скидка на товары в %
-        int totalCost = penCost + bookCost;
-        float discountAmount = totalCost * (discount / 100); // сумма скидки
-        float discountPrice = totalCost - discountAmount; //цена со скидкой
+        float penCost = 100f;
+        float bookCost = 200f;
+        float discount = 11f;
+        float totalCost = penCost + bookCost;
+        float discountAmount = totalCost * (discount / 100);
+        float discountPrice = totalCost - discountAmount;
         System.out.println("Общая стоимость товаров без скидки = " + totalCost);
         System.out.println("Сумма скидки = " + discountAmount);
         System.out.println("Общая стоимость товаров со скидкой = " + discountPrice);
@@ -36,21 +36,21 @@ public class VariablesTheme {
         System.out.println(" JJ  a     a  V  a     a");
 
         System.out.println("\n4. Вывод min и max значений целых числовых типов");
-        byte l = 127;
-        short m = 32_767;
-        int n = 2_147_483_647;
-        long o = 9_223_372_036_854_775_807l;
-        System.out.println("byte: " + l + ", " + (l + 1) + ", " + (l - 1));
-        System.out.println("short: " + m + ", " + (m + 1) + ", " + (m - 1));
-        System.out.println("int: " + n + ", " + (n + 1) + ", " + (n - 1));
-        System.out.println("long: " + o + ", " + (o + 1) + ", " + (o - 1));
+        byte maxByte = 127;
+        short maxShort = 32_767;
+        int maxInt = 2_147_483_647;
+        long maxLong = 9_223_372_036_854_775_807l;
+        System.out.println("byte: " + maxByte + ", " + (maxByte++) + ", " + (maxByte--));
+        System.out.println("short: " + maxShort + ", " + (maxShort++) + ", " + (maxShort--));
+        System.out.println("int: " + maxInt + ", " + (maxInt++) + ", " + (maxInt--));
+        System.out.println("long: " + maxLong + ", " + (maxLong++) + ", " + (maxLong--));
 
         System.out.println("\n5. Перестановка значений переменных");
         int num1 = 2;
         int num2 = 5;
         int num3;
         System.out.println("Исходные значения переменных: \n" + "Первая переменная = " + num1 + 
-            " , " + "вторая переменная = " + num2);
+                " , " + "вторая переменная = " + num2);
         System.out.println("Перестановка значений с помощью третьей переменной:");
         num3 = num1;
         num1 = num2;
@@ -68,55 +68,49 @@ public class VariablesTheme {
         System.out.println("Первая переменная = " + num1 + " , " + "вторая переменная = " + num2);
 
         System.out.println("\n6. Вывод символов и их кодов.");
-        char symbolNumber = '#';
-        char symbolAmpersand = '&';
-        char symbolAtSign = '@';
-        char symbolCaret = '^';
-        char symbolUnderscore = '_';
-        System.out.println((int) symbolNumber + " = " + symbolNumber);
-        System.out.println((int) symbolAmpersand + " = " + symbolAmpersand);
-        System.out.println((int) symbolAtSign + " = " + symbolAtSign);
-        System.out.println((int) symbolCaret + " = " + symbolCaret);
-        System.out.println((int) symbolUnderscore + " = " + symbolUnderscore);
+        char number = '#';
+        char ampersand = '&';
+        char atSign = '@';
+        char caret = '^';
+        char underscore = '_';
+        System.out.println((int) number + " = " + number);
+        System.out.println((int) ampersand + " = " + ampersand);
+        System.out.println((int) atSign + " = " + atSign);
+        System.out.println((int) caret + " = " + caret);
+        System.out.println((int) underscore + " = " + underscore);
 
         System.out.println("\n7. Вывод в консоль ASCII-арт Дюка");
-        // символ косой черты, слэш
-        char slash = 47;
-        // символ косой черты, обратный слэш
-        char reverseSlash = 92;
-        // символ нижнего подчеркивания
-        char underscore = 95;
-        // открывающая скобка
-        char openParenthesis = 40;
-        // закрывающая скобка
-        char closingParenthesis = 41;
+        char slash = '/';
+        char reverseSlash = '\\';
+        char openParenthesis = '(';
+        char closingParenthesis = ')';
         System.out.println("     " + slash + reverseSlash);
         System.out.println("    " + slash + "  " + reverseSlash);
         System.out.println("   " + slash + "" + underscore + "" + openParenthesis + " " + 
-            closingParenthesis + "" + reverseSlash);
+                closingParenthesis + "" + reverseSlash);
         System.out.println("  " + slash + "      " + reverseSlash);
         System.out.println(" " + slash + underscore + underscore + underscore + underscore + 
-            slash + reverseSlash + underscore + underscore + reverseSlash);
+                slash + reverseSlash + underscore + underscore + reverseSlash);
 
         System.out.println("\n8. Вывод количества сотен, десятков и единиц числа");
         int srcNum = 123;
-        int x = srcNum / 100;
-        int y = srcNum / 10 % 10;
-        int z = srcNum % 10;
-        int sum = x + y + z;
-        int multiply = x * y * z;
+        int hundred = srcNum / 100;
+        int ten = srcNum / 10 % 10;
+        int one = srcNum % 10;
+        int sum = hundred + ten + one;
+        int multiplication = hundred * ten * one;
         System.out.println("Число N содержит:");
-        System.out.println(x + " сотню");
-        System.out.println(y + " десятка");
-        System.out.println(z + " единицы");
+        System.out.println(hundred + " сотню");
+        System.out.println(ten + " десятка");
+        System.out.println(one + " единицы");
         System.out.println("Сумма его цифр = " + sum);
-        System.out.println("Произведение = " + multiply);
+        System.out.println("Произведение = " + multiplication);
 
         System.out.println("\n9. Вывод времени");
         int allSeconds = 86399;
         int hh = allSeconds / 3600;
-        int mm = (allSeconds - (hh * 3600)) / 60;
-        int cc = (allSeconds - (hh * 3600)) - (mm * 60);
+        int mm = allSeconds % 3600 / 60;
+        int cc = allSeconds % 3600 / 60 % 60;
         System.out.println(hh + ":" + mm + ":" + cc);
     }
 }
